e049de934b7532c236968e2ad8766643736743b3
zzadu <zzadu08@naver.com>
2022-05-23 09:36:17 +0000

    connect db

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+const db = {};
+let sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.user = require("./user.js")(sequelize, Sequelize);
+db.mountain = require("./mountain.js")(sequelize, Sequelize);
+db.bookmard = require("./bookmark.js")(sequelize, Sequelize);
+db.around = require("./around.js")(sequelize, Sequelize);
+db.post = require("./post.js")(sequelize, Sequelize);
+db.comment = require("./comment.js")(sequelize, Sequelize);
+
+
+module.exports = db;
