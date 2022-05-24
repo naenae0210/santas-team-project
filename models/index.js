dcea8f6a7dfa99f3e55eb033fa682373cf8f6863
zzadu <zzadu08@naver.com>
2022-05-24 14:40:40 +0000

    edit models

--- models/index.js
+++ models/index.js
@@ -10,7 +10,6 @@ db.Sequelize = Sequelize;
 db.User = require("./user.js")(sequelize, Sequelize);
 db.Mountain = require("./mountain.js")(sequelize, Sequelize);
 db.Bookmark = require("./bookmark.js")(sequelize, Sequelize);
-
 db.Around = require("./around.js")(sequelize, Sequelize);
 db.Post = require("./post.js")(sequelize, Sequelize);
 db.Comment = require("./comment.js")(sequelize, Sequelize);
