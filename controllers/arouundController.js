100aba08032fc10e20322a70f20144d917e2ef95
zzadu <zzadu08@naver.com>
2022-06-02 15:08:46 +0900

    test

--- controllers/arouundController.js
+++ controllers/arouundController.js
@@ -16,7 +16,7 @@ exports.searchAroundByName = async (req, res) => {
                 as: 'mountain',
                 attributes: ['number', 'name', 'address'],
                 through: {
-                    attributes: ['number']
+                    attributes: ['mountainNum']
                 },
                 where: {
                     name: {
