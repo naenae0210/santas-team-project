12517125ec67a3086ed2cc8279f357f95a95d3d4
zzadu <zzadu08@naver.com>
2022-06-03 14:01:06 +0900

    test

--- controllers/mountainInfoController.js
+++ controllers/mountainInfoController.js
@@ -2,23 +2,20 @@ const mysql = require("../models/index"),
     Mountain = mysql.Mountain,
     Around = mysql.Around;
 
-exports.showMountainInfo = async (req, res) => {
-	const mountainNum = req.params.number;
-	Mountain.findOne( {
-		where: {
-			number: mountainNum
-		}
-	})
-	.then(findMountain => {
-		res.render('mountainInfo', {mountain: findMountain
-		});
-	})
-	.catch(err => {
-        res.status(500).send({
-            message: err.message
-        });
-	});
-
-}
-
+	exports.showMountainInfo = async(req, res) => {
+		const mountainNum = req.params.number;
+		console.log(mountainNum);
+	
+		Mountain.findAll({
+			where: {
+				number: mountainNum
+			}
+		}).then(findMountain => {
+			res.render('mountainInfo', {mountain : findMountain});
+		}).catch(err => {
+			res.status(500).send({
+				message: err.message
+			});
+		})
+	}
 
