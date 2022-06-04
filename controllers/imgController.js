c554c84dc80bffe79572ec0ffe626b4ad0b622f8
zzadu <zzadu08@naver.com>
2022-06-04 23:36:21 +0900

    update

--- controllers/imgController.js
+++ controllers/imgController.js
@@ -24,7 +24,7 @@ exports.getImage = async (req, res) => {
             res.redirect("https://www.forest.go.kr/images/data/down/mountain/" + image[0]['imgfilename']);
         }
         else {
-            res.redirect("");
+            res.redirect("images/noimage.jpg");
         }
     })
 };
