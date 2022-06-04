a504e6d62efcc091f20ebc91267a85012490623b
zzadu <zzadu08@naver.com>
2022-06-04 23:46:30 +0900

    test

--- controllers/imgController.js
+++ controllers/imgController.js
@@ -1,5 +1,4 @@
-const request = require("request"),
-ip = require("ip");
+const request = require("request");
 
 const key = 'uztp5PFDDh%2BCHj3iQ8dpL9e5QQM3Dn3mIfzDaVG24UwPSyxzuDw3XB9pj6m6mh1DGfT3QuoU5HcE07vLuPPGdw%3D%3D';
 
@@ -23,7 +22,7 @@ exports.getImage = async (req, res) => {
             res.redirect("https://www.forest.go.kr/images/data/down/mountain/" + image[0]['imgfilename']);
         }
         else {
-            res.redirect(ip.address() + "/images/noimage.png");
+            res.redirect(":/images/noimage.png");
         }
     })
 };
