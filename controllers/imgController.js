720057876e5a3d6034be882221da3372feca8050
zzadu <zzadu08@naver.com>
2022-06-04 23:10:10 +0900

    test

--- controllers/imgController.js
+++ controllers/imgController.js
@@ -21,10 +21,10 @@ exports.getImage = async (req, res) => {
         console.log(image[0]);
 
         if (image != 'undefined' && image != null) {
-            req.redirect("www.forest.go.kr/images/data/down/mountain/" + image[0]['imgfilename']);
+            res.redirect("www.forest.go.kr/images/data/down/mountain/" + image[0]['imgfilename']);
         }
         else {
-            req.redirect("");
+            res.redirect("");
         }
     })
 };
