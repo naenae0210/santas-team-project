c7fe1663758f18c34e8cf307f0b2234f0a0c0db3
zzadu <zzadu08@naver.com>
2022-06-07 10:37:38 +0900

    update

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -44,7 +44,7 @@ exports.searchMountain = async (req, res) => {
 }
 
 exports.searchMountainByAdd = async (req, res) => {
-    const searchWord = req.body.searchWord;
+    const searchWord = switchWord(req.body.searchWord);
     console.log(searchWord);
     
     Mountain.findAll({
@@ -81,4 +81,47 @@ exports.searchMountainByAdd = async (req, res) => {
         })
         */
     })
+}
+
+function switchWord(searchWord) {
+    let sW = "";
+    switch (searchWord) {
+        case "서울특별시":
+            sW = "서울";
+            break;
+        case "인천광역시":
+            sW = "인천";
+            break;
+        case "대전광역시":
+            sW = "대전";
+            break;
+        case "광주광역시":
+            sW = "광주";
+            break;
+        case "대구광역시":
+            sW = "대구";
+            break;
+        case "울산광역시":
+            sW = "울산";
+            break;
+        case "부산광역시":
+            sW = "부산";
+            break;
+        case "제주특별자치도":
+            sW = "제주";
+            break;
+        case "전라도":
+            sW = "전라";
+            break;
+        case "충청도":
+            sW = "충청";
+            break;
+        case "경상도":
+            sW = "경상";
+            break;
+        default:
+            ;
+    }
+
+    return sW;
 }
