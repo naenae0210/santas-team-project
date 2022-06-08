f0688282d215c1f29d221c3d38f22a9fa84ee5b9
zzadu <zzadu08@naver.com>
2022-06-08 21:10:23 +0900

    test

--- controllers/imgController.js
+++ controllers/imgController.js
@@ -4,7 +4,7 @@ const key = 'JsYDcymAO%2BdqZ2KscehCHVe%2B83DUZExkHQdWA5S1eVJaBHVHjkaHVELfhZ3xX5p
 
 const add1 = 'http://apis.data.go.kr/1400000/service/cultureInfoService/mntInfoImgOpenAPI?mntiListNo='
     add2 = '&ServiceKey=',
-    add3 = '&_type=json';
+    add3 = '<response>&_type=json';
 
 exports.getImage = async (req, res) => {
 
