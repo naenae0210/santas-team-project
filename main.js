2affe04c3d2a8b8b8ab187ab3c9a3a01dff56fca
zzadu <zzadu08@naver.com>
2022-06-10 22:54:17 +0900

    update

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ const express = require("express"),
   userController = require("./controllers/userController"),
   postController = require("./controllers/postController"),
   db = require("./models/index"),
-  models = require("./models/index.js"),
   layouts = require("express-ejs-layouts"),
   mountainInfoController = require("./controllers/mountainInfoController"),
   imgController = require("./controllers/imgController"),
@@ -122,24 +121,6 @@ app.use("/", router);
 app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
 });
-
-
-// api test
-const request = require("request");
-
-const key = 'uztp5PFDDh%2BCHj3iQ8dpL9e5QQM3Dn3mIfzDaVG24UwPSyxzuDw3XB9pj6m6mh1DGfT3QuoU5HcE07vLuPPGdw%3D%3D';
-
-const add1 = 'http://apis.data.go.kr/1400000/service/cultureInfoService/mntInfoOpenAPI?searchWrd='
-    add2 = '&ServiceKey=',
-    add3 = '&numOfRows=10&pageNo=1&examdate=2017-12-27&_type=json';
-
-let address = add1 + encodeURI('북한산') + add2 + key + add3;
-
-app.get('/api', function(req, res, next) {
-  request(address, function(error, res, body) {
-    console.log(body);
-  })
-})
 
 // error
 
