00e4c48b924553c0611a8e44cbae4b18c9447e3b
naenae0210 <20201004@sunghin.ac.kr>
2022-06-11 00:08:26 +0000

    error

---
+++ controllers/commentController.js
@@ -0,0 +1,81 @@
+const db = require("../models/index"),
+    Comment = db.comment,
+    comment = db.comment,
+    Post = db.post,
+    post = db.post,
+
+    getCommentParams = body => {
+        return {
+            commentNum: commentNum,
+            postId: body.id,
+            commentDetail: body.commentDetail,
+        };
+    };
+
+module.exports = {
+    create: async (req, res, next) => {
+        let postId = req.params.id;
+        let commentParams = getCommentParams(req.body);
+        try {
+            let comment = await Comment.create(commentParams);
+            console.log(comment);
+            res.json(comment);
+            res.locals.redirect = `/posts/${postId}`;
+            res.locals.comment = comment;
+            next();
+        } catch (error) {
+            console.log(`Error saving comment: ${error.messgae}`);
+            next(error);
+        };
+    },
+
+    getComment: async (req, res, next) => {
+        try {
+            const post = Post.findOne({});
+            const comments = await post.getComment();
+            console.log(comments);
+            next();
+
+        } catch {
+            console.log(`Error finding comment: ${error.messgae}`);
+            next(error);
+        }
+    },
+
+    update: async (req, res, next) => {
+        let postId = req.params.id;
+        try {
+            let comment = await Comment.update({
+                commentDeatail: req.body.commentDeatil
+            }, {
+                where: {
+                    postId: req.params.id
+                }
+            });
+            res.json(comment);
+            res.locals.redirect = `/posts/${postId}`;
+            res.locals.comment = comment;
+            next();
+
+        } catch {
+            console.log(`Error updating comment : ${error.messgae}`);
+            next();
+        }
+    },
+
+    delete: async (req, res, next) => {
+        let postId = req.params.id;
+        try {
+            let comment = await Comment.destroy({
+                where: {
+                    postId: req.params.id
+                }
+            });
+            res.json(comment);
+            next();
+        } catch (error) {
+            console.log(`Error deleting comment: ${error.messgae}`);
+            next();
+        };
+    },
+};
