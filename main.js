530487a96c67b2eccfcb7cda39aa01ab70aba8dd
zzadu <zzadu08@naver.com>
2022-06-12 18:18:29 +0900

    update

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ router.get("/around/:region", aroundController.searchAroundByAdd);
 router.get("/bookmark", bookmarkController.allBookmark);
 
 router.get("/community", homeController.showCommunity);
-router.get("/delPost", homeController.showMyPost);
+router.get("/delPost", postController.postById);
 router.get("/myProfile", homeController.showMyProfile);
 router.get("/mountain", mountainController.allMountain, bookmarkController.isBookmark);
 router.get("/mountain/:region", mountainController.searchMountainByAdd, bookmarkController.isBookmark);
