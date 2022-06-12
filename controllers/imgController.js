e9bdab96bc792b50f4cab2a9244a1e65853d29d4
zzadu <zzadu08@naver.com>
2022-06-12 17:32:10 +0900

    test

--- controllers/imgController.js
+++ controllers/imgController.js
@@ -2,7 +2,7 @@ const request = require("request"),
     fetch = require("node-fetch");
 
 
-const key = 'JsYDcymAO%2BdqZ2KscehCHVe%2B83DUZExkHQdWA5S1eVJaBHVHjkaHVELfhZ3xX5pGknpwTb6wRVIg4fbsQOV%2Fnw%3D%3D';
+const key = 'HalmOJvtllDf2LtbeFjdxEyAM5e6okRMDLyG9YLH2OQzltWsOAkTxoKyA%2BMOZ0WZ5glQOzqEK8SB0CX3s0zbMQ%3D%3D';
 
 const add1 = 'http://apis.data.go.kr/1400000/service/cultureInfoService/mntInfoImgOpenAPI?mntiListNo='
     add2 = '&ServiceKey=',
@@ -13,6 +13,10 @@ exports.getImage = async (req, res) => {
     const address = add1 + req.params.number + add2 + key + add3;
 
     fetch(address).then(result => {
+
+        fetch(result.url).then(r => {
+            console.log(r);
+        })
 
         console.log(result.url);
 
